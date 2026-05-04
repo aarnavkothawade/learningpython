9093601779b4f7349030cf4cb153f6b6c51700e1
Aarnav Kothawade <aarnavkothawade@gmail.com>
2026-05-04 22:49:52 +0530

    Add files via upload

---
+++ 1st Chapter Starting with Py/first.py
@@ -0,0 +1,15 @@
+print("Hello World") 
+#This is a comment
+
+a = 45 #integer
+b = "Aarnav" #string
+c = 4.55 #float
+# Therefore dynamically allocates datatype
+
+t = True
+f = False
+# print(type(t))
+
+is_completed = True 
+print(is_completed)
+
